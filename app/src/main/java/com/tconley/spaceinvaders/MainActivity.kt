a15acceeb0196070d65681518203941254bef6c0
Tyler Conley <tylerjacobconley1@gmail.com>
2025-03-19 22:36:27 -0700

    adding main screen

--- app/src/main/java/com/tconley/spaceinvaders/MainActivity.kt
+++ app/src/main/java/com/tconley/spaceinvaders/MainActivity.kt
@@ -1,47 +1,63 @@
 package com.tconley.spaceinvaders
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tconley.spaceinvaders.ui.theme.SpaceInvadersTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SpaceInvadersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    val context = androidx.compose.ui.platform.LocalContext.current
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to Space Invaders!")
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpaceInvadersTheme {
-        Greeting("Android")
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = {
+                // Start SpaceInvadersActivity
+                val intent = Intent(context, SpaceInvadersActivity::class.java)
+                context.startActivity(intent)
+            }) {
+                Text(text = "Start Game")
+            }
+        }
     }
 }
